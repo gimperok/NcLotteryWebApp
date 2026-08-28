086db10594d3d2700f10e5c521c505109b17fe18
Alexei <gimp24@mail.ru>
2026-08-27 23:19:20 -0400

    - Added the LotteryFactory class, which includes the CreateLottery method.

---
+++ Factories/LotteryFactory.cs
@@ -0,0 +1,17 @@
+﻿using NcLotteryWebApp.Models;
+
+namespace NcLotteryWebApp.Factories
+{
+    public class LotteryFactory
+    {
+        public Lottery? CreateLottery(string choice)
+        {
+            return choice.ToLower() switch
+            {
+                "1" or "powerball" => new PowerballLottery(),
+                "2" or "megamillions" => new MegaMillionsLottery(),
+                _ => null
+            };
+        }
+    }
+}
